b965c0953b7f0ed5076b4c56aa1567642d3c5ec6
tasuwo <kamuhata.you@gmail.com>
2018-07-08 00:55:22 +0900

    :wrench: モード指定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, '../dist'),
